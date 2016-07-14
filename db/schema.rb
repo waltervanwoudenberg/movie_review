452bb152af6cd92077cf14bd049a0936efc574b8
Walter van Woudenberg <vanwoudenbergwalter@gmail.com>
2016-07-14 20:39:20 +0200

    Convert review ratings to stars

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714170117) do
+ActiveRecord::Schema.define(version: 20160714171030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160714170117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
